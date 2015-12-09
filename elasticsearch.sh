4c478e0ace616998bcb4c6329e710a2a054c2c6a
Andrew Grant <andrew.grant@codecomputerlove.com>
2015-12-09 14:27:36 +0000

    Bumped to version 2.0.0

--- elasticsearch.sh
+++ elasticsearch.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-
+mkdir -p /elasticsearch/logs
+mkdir -p /elasticsearch/plugins
 chown elasticsearch:elasticsearch /data
+chown elasticsearch:elasticsearch /elasticsearch/logs
+chown elasticsearch:elasticsearch /elasticsearch/plugins
+chown elasticsearch:elasticsearch /elasticsearch/config
 chmod 0755 /data
+chmod 0755 /elasticsearch/logs
+chmod 0755 /elasticsearch/plugins
+chmod 0755 /elasticsearch/config
 
 exec /sbin/setuser elasticsearch /elasticsearch/bin/elasticsearch
 
